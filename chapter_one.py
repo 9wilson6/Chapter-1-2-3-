d69613c5dcd2e69b529140bed14ddbbcd7bddbdf
9wilson6 <gatheruwilson@gmail.com>
2019-09-11 15:31:56 +0300

    solution to chapter one exercise

---
+++ chapter_one.py
@@ -0,0 +1,71 @@
+Q1.
+
+* #operator
+'hello' #value
+-88.8 #value
+- #operator
+/ #operator
++ #operator
+5 #value
+
+Q2.
+
+spam #variable
+'spam' #string
+
+Q3.
+
+ integer, floating-Point, & String data types
+
+Q4.
+
+expressions are just values combined with operators.
+they always evaluate down to a single value.
+
+Q5.
+
+an expression evalautes to a value e.g x + 10 -3=19,
+a statement performs some action eg assingment and print statements.
+
+Q6.
+# >>> b=20
+# >>> b+1
+# 21
+# >>> b
+# 20
+# >>>
+
+Q7.
+
+>>> 'spam' + 'spamspam'
+'spamspamspam'
+
+>>> 'spam' * 3
+'spamspamspam'
+
+Q8.
+a variable can:
+    be a word
+    start with a letter but not a number
+    contain letters numbers and underscore but not any other special character.
+egg is valid as it is a word but 100 begins with a number hence it is invalid.
+
+
+Q9.
+
+int() # to get the integer value
+str() # to get the string value
+float() # to get the floating-point value of a number.
+
+Q10.
+'I have eaten ' + 99 + ' burritos.' # causes an error because it attempts to add an integer to a string
+# if the expressons was to work, the integer (99) should be explicitly converted to a string.
+
+
+
+
+
+
+
+
+
