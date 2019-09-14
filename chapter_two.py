f4b9f3b14905779dd2d0736a40805991053508d1
9wilson6 <gatheruwilson@gmail.com>
2019-09-14 21:42:42 +0300

    Chapter two answers

---
+++ chapter_two.py
@@ -0,0 +1,83 @@
+Q1. True & False
+Q2. or, not, & and
+Q3.
+and truth table
+    true and true => true
+    true and false =>false
+    false and true =>false
+    false and false =>false
+or truth table
+    true or true => true
+    true or false =>true
+    false or true =>true
+    false or false =>false
+not truth table
+    not true =>false
+    not false=>true
+>>> (4>5) and (3==5)
+False
+>>> not (5 > 4)
+False
+>>> (5 > 4) or (3 == 5)
+True
+>>> not ((5 > 4) or (3 == 5))
+False
+>>> (True and True) and (True == False)
+False
+>>> (not False) or (not True)
+True
+Q5. == Equal to
+    > greater than
+    < less than
+    >= greater than or equal to
+    <=less than or equal to
+    != Not equal to
+Q6.equal to operator is used to check if two variables are equal while the assignment operator is used to
+assign a value to a variable.
+
+Q7. a condition is a situation where the results of a given event are uncertain and two or more outcomes this outcomes
+can be viewed as conditions. they are used when there is more than one outcome to a certain event.
+Q8.
+spam = 0                    #block 1
+if spam == 10:              #block 1
+    print('eggs')           #block 2
+    if spam > 5:            #block 2
+        print('bacon')      #block 3
+    else:                   #block 2
+        print('ham')        #block 3
+    print('spam')           #block 2
+print('spam')               #block 1
+
+Q9.
+spam=0
+
+if spam ==1:
+    print("Hello")
+elif spam==2:
+    print("Howday")
+else:
+    print("Greetings")
+
+Q10.
+ctr + c
+
+Q11.
+break exits the loop while continue is used to skip code within the loop.
+
+
+Q12.
+range(10) implies 0 to 9
+range(0, 10) implies 0 to 9 starting from 0
+range(0,10,1) implies 0 to 9 with a step of 1
+Q13. for i in range(11):
+    print(i)
+i=0
+while i<=10:
+    print(i)
+    i++
+Q14.
+import spam
+spam.bacon()
+
+
+    
