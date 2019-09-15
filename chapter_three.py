e7b2948c272578c3a0f56b90d0c0383e288fa72e
9wilson6 <gatheruwilson@gmail.com>
2019-09-15 10:46:33 +0300

    Chapter three solutions

---
+++ chapter_three.py
@@ -0,0 +1,21 @@
+Q1. since they group code that get's executed multiple times, we don't have to copy
+paste the code multiples times which also make it easy to read and debug.
+Q2.When the function is called
+Q3. def the  functionName
+Q4. A function is a gruop of code that is excuted as a unit while a function call
+excutes the function.
+Q5. 1 global scope and it is created when your program begins.
+    1 local scope and it is created whenever a function is called..
+Q6. When the function returns, variables in local scope are destroyed and forgotten.
+Q7. A return value is the value that a function call evaluates to.
+    Yes they can be part of an expresion.
+Q8. None.
+Q9. by using a global statement on that variable.
+Q10. none is the only value of NoneType data type.
+Q11. That import statement imports a module named areallyourpetsnamederic.
+Q12. import spam
+    spam.bacon()
+Q13. by using try and except statements.
+Q14.try holds: the code that could result into an error
+    except holds: the code to be excuted in the instance
+    of an error from the try block
